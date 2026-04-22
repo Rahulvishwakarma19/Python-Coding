824d9a0ce3c7a7b6400b729586edfa28e2b9eefc
Rahul Vishwakarma <rahulvishwakarma8171@gmail.com>
2026-04-22 17:03:45 +0530

    python_p

---
+++ First_Day.py
@@ -0,0 +1,5 @@
+name = "rahul"
+age = 27
+print("My name is:",name)
+print("My age is:",age)
+print("My name is ",name," and my age is ",age)
--- rahul.py
+++ rahul.py
@@ -1,23 +1,10 @@
+print("Hello World")
+#type Conversion in python..
 
-# print("hello rahul vishwakarm world")
-# print("rahul")
-# x=10
-# y=20
-# z=x+y
-# print("total of the ",z)
-# a=int(input("Enater the numbr: "))
-# if(a%2==0):
-#     print("Even number")
-# else:
-#     print("Odd number")
+mytuple = ("apple", "banana", "cherry")
+mylist = list(mytuple)
+mylist[1] = "kiwi"
+mytuple = tuple(mylist)
 
-
-def Ram():
-    print("Jay shree Ram..")
-
-def Syam():
-    print("Jay shree Krishana")
-
-def Mahadev():
-    print("Jay Bholenath..")
-
+print(type(mytuple))
+print(mytuple)
