7b27e2a23c443905c92899f196e9381dbd555570
Rahul Vishwakarma <rahulvishwakarma8171@gmail.com>
2026-07-23 17:00:35 +0530

    python

--- First_Day.py
+++ First_Day.py
@@ -3,3 +3,9 @@ age = 27
 print("My name is:",name)
 print("My age is:",age)
 print("My name is ",name," and my age is ",age)
+
+num = int(input("Enter the number: "))
+if(num%2==0):
+    print(f"{num} even number")
+else:
+    print(f"{num} odd number")
